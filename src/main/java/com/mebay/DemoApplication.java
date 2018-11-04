5edcb1bedded3315625f419ffd7cb6598d5cf056
TheLeFS <43313900+TheLeFS@users.noreply.github.com>
2018-11-04 21:01:53 +0800

    测试

--- src/main/java/com/mebay/DemoApplication.java
+++ src/main/java/com/mebay/DemoApplication.java
@@ -17,7 +17,6 @@ import java.util.logging.Logger;
  */
 @SpringBootApplication
 // 添加servlet组件扫描，使得Spring能够扫描到我们编写的servlet和filter
-
 @ServletComponentScan
 //@EnableTransactionManagement
 @MapperScan("com.mebay.mapper")
